6443d578a4b3d97c2e91a1d0cb872e3f184fdaa2
Viktor Müller <muller.viktor.muller@gmail.com>
2020-02-14 15:49:55 +0100

    Added empty model classes for admin

--- street_site/street/admin.py
+++ street_site/street/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import *
+
 # Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(Question)
+admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(Like)
--- street_site/street/models.py
+++ street_site/street/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    # todo
+    pass
+
+
+class Question(models.Model):
+    # todo
+    pass
+
+
+class Category(models.Model):
+    # todo
+    pass
+
+
+class Tag(models.Model):
+    # todo
+    pass
+
+
+class Like(models.Model):
+    # todo
+    pass
